ec58ba1763df166ef50da6491d3831d4911e858e
Omkar-Dops <dhotreomkar58@gmail.com>
2024-10-06 20:53:40 +0530

    Add files via upload

---
+++ OtpGenerator.java
@@ -0,0 +1,15 @@
+class OtpGenerator
+{
+	public static void main(String[] args)
+	{
+		String op=generateOtp();
+			System.out.println(op);
+	}
+	public static String generateOtp()
+	{
+		String otp="";
+		for(int i=1;i<=4;i++)
+		otp+=((int)(Math.random()*9));
+		return otp;
+	}
+}
